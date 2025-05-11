7bf62ef6eaf01f4cf2d51cddefe854e2135c0b85
Sagar-Mallik <mrsagarxt@gmail.com>
2025-05-12 00:35:25 +0530

    Added Mail Service & OTP Verification

--- src/main/java/com/sagar/service/MailService.java
+++ src/main/java/com/sagar/service/MailService.java
@@ -1,8 +1,11 @@
 package com.sagar.service;
 
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +14,24 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendOtp(String email, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Your OTP for Signup");
-        message.setText("Your OTP is: " + otp);
-        mailSender.send(message);
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public boolean sendMail(String to, String subject, String body) {
+        try{
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            helper.setFrom(fromEmail);
+
+            mailSender.send(message);
+            return true;
+        } catch (Exception e) {
+            System.err.println("Failed to send email: " + e.getMessage());
+            return false;
+        }
     }
 }
